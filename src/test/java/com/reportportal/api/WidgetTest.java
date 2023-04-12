3c7d1e4bcdc3788ec94cddb1d91294ab58021619
Ivan_Bazhai <bazhai01@kits.kfplc.com>
2023-04-12 09:43:55 +0300

    new branch

---
+++ src/test/java/com/reportportal/api/WidgetTest.java
@@ -0,0 +1,16 @@
+package com.reportportal.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+public class WidgetTest {
+
+    private static final Logger log = LoggerFactory.getLogger(WidgetTest.class);
+
+    @Test
+    public void verifyCreateWidget() {
+        log.info("sdcsdcdc");
+        assert false;
+    }
+}
